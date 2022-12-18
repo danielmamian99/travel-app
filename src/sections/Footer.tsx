a722a72bc067063e47f2aab16213e12c4092f1cf
danielmamian99 <danimaster99@gmail.com>
2022-12-18 11:26:59 -0500

    footer responsive

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -12,12 +12,36 @@ export const Footer = () => {
             ></img>
           </figure>
           <article className="flex justify-center items-center pl-5 w-1/2 md:w-1/3">
-            <p className="text-lg md:text-xl lg:text-2xl">
-              #ViajaAlMomento
-            </p>
+            <p className="text-lg md:text-xl lg:text-2xl">#ViajaAlMomento</p>
           </article>
         </div>
-        <figure className="mt-8 w-64 md:w-80 lg:w-[26rem]">
+        <div className="hidden md:flex md:justify-center">
+          <div className="flex justify-center w-1/2">
+            <figure className="mt-8 w-64 md:w-80">
+              <img
+                src="/images/footer/reservas/reserva.png"
+                srcSet="/images/footer/reservas/reserva.png 300w, /images/footer/reservas/reserva2.png 768w, /images/footer/reservas/reserva3.png 1280w"
+                sizes="(max-width: 300px) 300px, (max-width: 768px) 768px, 1280px"
+                alt="Wonder Travel reservas"
+              />
+            </figure>
+          </div>
+
+          <div className="flex flex-col justify-center w-1/2">
+            <p className="underline text-center text-large md:text-2xl lg:text4xl">
+              Miembros de:
+            </p>
+            <figure className="flex justify-center">
+              <img
+                src="/images/footer/acotur/acotur.png"
+                srcSet="/images/footer/acotur/acotur.png 300w, /images/footer/acotur/acotur2.png 768w, /images/footer/acotur/acotur3.png 1280w"
+                sizes="(max-width: 300px) 300px, (max-width: 768px) 768px, 1280px"
+                alt="Acotur"
+              />
+            </figure>
+          </div>
+        </div>
+        <figure className="mt-8 w-64 md:hidden">
           <img
             src="/images/footer/reservas/reserva.png"
             srcSet="/images/footer/reservas/reserva.png 300w, /images/footer/reservas/reserva2.png 768w, /images/footer/reservas/reserva3.png 1280w"
@@ -25,9 +49,11 @@ export const Footer = () => {
             alt="Wonder Travel reservas"
           />
         </figure>
-        <div className="flex flex-col justify-center m-4">
-          <p className="underline text-center text-large md:text-2xl lg:text4xl">Miembros de:</p>
-          <figure>
+        <div className="flex flex-col justify-center m-4 md:hidden">
+          <p className="underline text-center text-large md:text-2xl lg:text4xl">
+            Miembros de:
+          </p>
+          <figure className="flex justify-center">
             <img
               src="/images/footer/acotur/acotur.png"
               srcSet="/images/footer/acotur/acotur.png 300w, /images/footer/acotur/acotur2.png 768w, /images/footer/acotur/acotur3.png 1280w"
