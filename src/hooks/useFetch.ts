9ae6856889b46068f11574bc909d4317c6fb73ed
danielmamian99 <danimaster99@gmail.com>
2022-12-18 14:35:26 -0500

    fetch product

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,15 +1,15 @@
-import { useState, useEffect } from 'react';
-import { data } from '../api/data';
-import { Itinerary } from '../interfaces';
+import { useState, useEffect } from "react";
+import { data } from "../api/data";
+import { Itinerary } from "../interfaces";
 
-export const useFetch = ():Itinerary[] | {} => {
-    const [itinerary, setItinerary] = useState({})
-    useEffect(() => {
-        // fetch("/src/api/data.json")
-        // .then((response) => response.json())
-        // .then((data) => setItinerary(data))
-        setItinerary(data) //Because in production we cannot consume local api
-    }, [])
-    
-    return itinerary;
-}
+export const useFetch = (): Itinerary[] | {} => {
+  const [itinerary, setItinerary] = useState({});
+  useEffect(() => {
+    // fetch("/src/api/data.json")
+    // .then((response) => response.json())
+    // .then((data) => setItinerary(data))
+    setItinerary(data); //Because in production we cannot consume local api
+  }, []);
+
+  return itinerary;
+};
